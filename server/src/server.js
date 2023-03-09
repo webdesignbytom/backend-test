5bd07c83ec8956f819c80162e16842549f511b1d
Thomas Brockington <xtombrock1989@gmail.com>
2023-03-09 23:38:32 +0000

    save

--- server/src/server.js
+++ server/src/server.js
@@ -21,6 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT 
 const HTTP_URL = process.env.HTTP_URL  || 'https://backend-test-it01me3aa-webdesignbytom.vercel.app/'
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 // Server interface page
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
